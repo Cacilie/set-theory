d103cd7d8dd4d4ea0886756b98a214a7d188525f
E. A. Arroyo <hi.ed@hotmail.com>
2025-05-24 14:43:59 -0400

    Adds new developments to the index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import SetOf from "./classes/SetOf.ts";
+import power_set from "./utils/powerset.ts";
+import array_to_set from "./utils/utils.ts";
 
 export default {
-    SetOf
+    SetOf,
+    power_set,
+    array_to_set,
 }
