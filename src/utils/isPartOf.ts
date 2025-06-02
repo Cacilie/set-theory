a17ab16ab331370be28106ec1d143e4241d1b81d
E. A. Arroyo <hi.ed@hotmail.com>
2025-06-01 23:20:04 -0400

    Adds Difference operation

--- src/utils/isPartOf.ts
+++ src/utils/isPartOf.ts
@@ -1,11 +1,11 @@
-import AreArraySetsEqual from "./AreArraySetsEqual.ts";
+import areArraySetsEqual from "./areArraySetsEqual.ts";
 
 export default function isPartOf(A: unknown | [], B: unknown[]): boolean {
 
     return B.some( m_b => {
         
         if(m_b instanceof Array){
-            return AreArraySetsEqual(m_b, A as Array<unknown>);
+            return areArraySetsEqual(m_b, A as Array<unknown>);
         }
         return m_b === A
     })
