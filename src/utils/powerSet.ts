ce23740ee7f54b939e9618aaf2ef854d9d6255a0
E. A. Arroyo <hi.ed@hotmail.com>
2025-06-21 14:31:32 -0400

    Fix powerSet bugs

--- src/utils/powerSet.ts
+++ src/utils/powerSet.ts
@@ -1,19 +1,20 @@
 
 import type { SetType } from "../index.ts";
+import IsArraySetPartOf from "./isArraySetPartOf.ts";
 
 export default function powerSet<T>(set: SetType<T>): SetType<T> {
 
-    const solution :  Array<T[] | null[]> = []
+    const solution :  Array<T[]> = []
 
-    solution.push([null])
+    solution.push([])
 
     set.forEach(pivot => {
         solution.forEach(member => {
-            const copy = (member as Array<T> )[0];
-            const tmp = [];
-            if (copy) tmp.push(copy);
+            const copy = (member as Array<T> )
+            let tmp : T[] = [];
+            if (copy.length > 0) tmp = [...copy]
             tmp.push(pivot);
-            solution.push(tmp);
+            if(!IsArraySetPartOf<T[]>(tmp, solution)) solution.push(tmp);
         })
     })
 
