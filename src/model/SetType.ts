f9340a5f73a6536d185f3dc8d3d9983b63e98a70
E. A. Arroyo <hi.ed@hotmail.com>
2026-01-21 14:14:48 -0500

    Add docs to setType

--- src/model/SetType.ts
+++ src/model/SetType.ts
@@ -1,1 +1,7 @@
+/**
+ * Represents a generic set type, which is internally modeled as an array of elements.
+ * This type alias is used throughout the set-theory library to maintain consistency
+ * when referring to the underlying data structure of a set.
+ * @template T The type of elements contained within the set.
+ */
 export type SetType<T> = Array<T>
